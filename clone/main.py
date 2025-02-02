1cf561d41c7375cca34dde07de6d225e2ebbab00
Florian SIPP <florian.sipp@chuv.ch>
2025-02-02 21:16:11 +0100

    Improved mni registration for t1 and ct TODO : explore ants function and compare result

--- clone/main.py
+++ clone/main.py
@@ -1,37 +1,51 @@
 import os
 import argparse
 from pathlib import Path
-from clone.operations import crop_image, move_image, coregister_images, subtract_image, threshold_image, apply_transformation2image, extract_brain, mask_image, cortical_reconstruction
+from clone.operations import crop_image, move_image, coregister_images, subtract_image, threshold_image, apply_transformation2image, extract_brain, extract_brain2, mask_image, cortical_reconstruction, transform_coordinates, register_ct_to_mni, register_mri_to_mni
 from clone.utility import read_config_file
 from clone.subject import Subject
 
 def run_operation(operation, subject: Subject):    
-    if operation['workdir']:
-        workdir = subject.get_folder(operation['workdir'])
-        print(f"Changing directory to {workdir}")
-        os.chdir(workdir)
-
-    files = [subject.get_file(f.replace("${name}", subject.get_subject_name())) for f in operation['files'][:-1]] + \
-            [operation['files'][-1].replace("${name}", subject.get_subject_name()).replace("${subj_dir}", str(subject.folder_path))]
+    # Store the original working directory
+    original_dir = os.getcwd()
     
-    if operation['type'] == 'crop':
-        crop_image(*files)
-    elif operation['type'] == 'move':
-        move_image(*files)    
-    elif operation['type'] == 'coregister':
-        coregister_images(*files)
-    elif operation['type'] == 'subtract':
-        subtract_image(*files)
-    elif operation['type'] == 'threshold':
-        threshold_image(*files)
-    elif operation['type'] == 'apply_transformation':
-        apply_transformation2image(*files)
-    elif operation['type'] == 'extract_brain':
-        extract_brain(*files)
-    elif operation['type'] == 'mask':
-        mask_image(*files)
-    elif operation['type'] == 'reconstruct':
-        cortical_reconstruction(*files)
+    try:
+        if operation['workdir']:
+            workdir = subject.get_folder(operation['workdir'])
+            print(f"Changing directory to {workdir}")
+            os.chdir(workdir)
+
+        files = [subject.get_file(f.replace("${name}", subject.get_subject_name())) for f in operation['files'][:-1]] + \
+                [operation['files'][-1].replace("${name}", subject.get_subject_name()).replace("${subj_dir}", str(subject.folder_path))]
+        
+        if operation['type'] == 'crop':
+            crop_image(*files)
+        elif operation['type'] == 'move':
+            move_image(*files)    
+        elif operation['type'] == 'coregister':
+            coregister_images(*files)
+        elif operation['type'] == 'subtract':
+            subtract_image(*files)
+        elif operation['type'] == 'threshold':
+            threshold_image(*files)
+        elif operation['type'] == 'apply_transformation':
+            apply_transformation2image(*files)
+        elif operation['type'] == 'extract_brain':
+            extract_brain(*files)
+        elif operation['type'] == 'extract_brain2':
+            extract_brain2(*files)
+        elif operation['type'] == 'mask':
+            mask_image(*files)
+        elif operation['type'] == 'reconstruct':
+            cortical_reconstruction(*files)
+        elif operation['type'] == 'register_ct_to_mni':
+            register_ct_to_mni(*files)
+        elif operation['type'] == 'register_mri_to_mni':
+            register_mri_to_mni(*files)
+
+    finally:
+        # Always return to the original directory
+        os.chdir(original_dir)
 
 def run_stage(stage, subject):
     print(f"Running stage: {stage['name']}")
@@ -43,6 +57,8 @@ def main():
     argsparser.add_argument('--subjects', nargs='+', help='List of subjects.')
     argsparser.add_argument('--create-folder', action='store_true', help='Create folder for the subject(s) provided in the list.')
     argsparser.add_argument('--stages', nargs='+', help='Name of the stage to run. If not provided, all stages will be run.')
+    argsparser.add_argument('--transform-coordinates', type=str, 
+                           help='Path to the JSON file containing coordinates to transform')
 
     args = argsparser.parse_args()
     config_data = read_config_file("config/config.yaml")
@@ -62,6 +78,26 @@ def main():
             print("Please provide the list of subject names.")
             return
 
+    # Handle transform coordinates independently
+    if args.transform_coordinates:
+        if not args.subjects:
+            print("Please provide the subject name(s) for coordinate transformation.")
+            return
+            
+        for subject_name in args.subjects:
+            subject_folder = output_directory_path / subject_name
+            transform_mat = subject_folder / 'processed_tmp' / f'MNI_{subject_name}_T1.mat'
+
+            if not transform_mat.exists():
+                print(f"Transformation matrix not found for subject {subject_name}")
+                continue
+            
+            output_json = Path(args.transform_coordinates).parent / f'MNI_{Path(args.transform_coordinates).stem}.json'
+            transform_coordinates(args.transform_coordinates, transform_mat, output_json)
+            print(f"Transformed coordinates saved to {output_json}")
+        return  # Exit after coordinate transformation
+
+    # Handle stages only if transform_coordinates wasn't specified
     if args.subjects:
         for subject_name in args.subjects:
             subject_folder = output_directory_path / subject_name
--- clone/operations.py
+++ clone/operations.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 from clone.utility import execute_command
+import json
+import numpy as np
+import os
+import subprocess
 
 def crop_image(input_file: Path, output_filename: str) -> Path:
     input_file = Path(input_file)
@@ -47,35 +51,6 @@ def coregister_images(input_file: Path, ref_file: Path, output_file_name: str) -
         "-dof", "6",
         "-interp", "sinc",
         "-datatype", "int"
-    ])
-
-def coregister_to_mni(input_file: Path, output_file_name: str) -> None:
-    input_file = Path(input_file)
-    ref_file = Path("/usr/local/fsl/data/standard/MNI152_T1_1mm.nii.gz")
-
-    if not input_file.exists():
-        print(f"Input file {input_file} does not exist.")
-        return
-    if not ref_file.exists():
-        print(f"Reference file {ref_file} does not exist.")
-        return
-
-    print(f"Registering {input_file.stem} to {ref_file.stem} => {output_file_name}")
-    execute_command([
-        "/usr/local/fsl/bin/flirt",
-        "-in", input_file,
-        "-ref", ref_file,
-        "-out", output_file_name,
-        "-omat", output_file_name + ".mat",
-        "-bins", "256",
-        "-cost", "mutualinfo",
-        "-searchrx", "-180", "180",
-        "-searchry", "-180", "180",
-        "-searchrz", "-180", "180",
-        "-dof", "12",
-        "-interp", "sinc",
-        "-sincwidth", "7", 
-        "-sincwindow", "hanning"
     ])
 
 def subtract_image(input_file: Path, mask_file: Path, output_file_name: str) -> None:
@@ -138,6 +113,15 @@ def extract_brain(input_file: Path, output_file: Path):
         "bet", input_file.stem, output_file.stem, "-f", "0.45", "-g", "0", "-m"
     ])
 
+def extract_brain2(input_file: Path, output_file: Path):
+    input_file = Path(input_file)
+    output_file = Path(output_file)
+
+    # Use BET to preserve screws and enhance visibility of relevant structures
+    execute_command([
+        "bet", input_file.stem, output_file.stem, "-f", "0.25", "-g", "0"
+    ])
+
 def mask_image(input_file: Path, mask_file: Path, output_file_name: str):
     input_file = Path(input_file)
     mask_file = Path(mask_file)
@@ -165,3 +149,247 @@ def cortical_reconstruction(input_file: Path, fs_output_dir: str):
     execute_command([
         "recon-all", "-sd", fs_subject_dir.parent, "-s", fs_subject_dir.stem, "-i", input_file, "-all"
     ])
+
+def transform_coordinates(input_json: Path, transformation_matrix: Path, output_json: str) -> None:
+    """
+    Transform electrode coordinates from subject space to MNI space
+    using the transformation matrix from the registration pipeline.
+    
+    Note: This function assumes electrode coordinates are in image space
+    and intentionally ignores translations from the transformation matrix
+    since we only want to apply rotation and scaling components.
+    """
+    # Read the transformation matrix
+    with open(transformation_matrix, 'r') as f:
+        matrix_lines = f.readlines()
+        matrix = np.array([[float(num) for num in line.strip().split()] for line in matrix_lines])
+
+    # Zero out translations since electrodes are marked in image space
+    # and we only want to apply rotation/scaling
+    matrix[:3, 3] = 0
+
+    # Read the JSON file with electrode coordinates
+    with open(input_json, 'r') as f:
+        data = json.load(f)
+
+    # Transform each coordinate
+    for markup in data.get('markups', []):
+        for point in markup.get('controlPoints', []):
+            # Get the coordinate and make it homogeneous (add 1 as 4th component)
+            coord = np.array([
+                point['position'][0], 
+                point['position'][1], 
+                point['position'][2],
+                1.0
+            ])
+            
+            # Apply transformation matrix (rotation and scaling only)
+            mni_coord = matrix @ coord
+            
+            # Update the position (remove homogeneous component)
+            point['position'] = [
+                float(mni_coord[0]), 
+                float(mni_coord[1]), 
+                float(mni_coord[2])
+            ]
+            point['description'] = point.get('description', '') + ' (MNI)'
+
+    # Save transformed coordinates
+    with open(output_json, 'w') as f:
+        json.dump(data, f, indent=2)
+
+def register_mri_to_mni(input_file: Path, output_file_name: str) -> None:
+    """
+    Register a T1 MRI image to MNI space using FSL FLIRT. The registration is done in two stages:
+    1. Rigid registration (6 DOF) to get a rough alignment
+    2. Affine registration (12 DOF) initialized with the rigid transform for fine-tuning
+    
+    Args:
+        input_file: Path to the input T1 MRI image
+        output_file_name: Name of the output file (without extension). Will create:
+            - {output_file_name}_rigid.mat: Rigid transformation matrix
+            - {output_file_name}_rigid: Rigidly registered image
+            - {output_file_name}.mat: Final affine transformation matrix  
+            - {output_file_name}: Final registered image
+    """
+    input_file = Path(input_file)
+    ref_file = Path(f"{os.environ.get('FSLDIR')}/data/standard/MNI152_T1_0.5mm.nii.gz")
+
+    if not input_file.exists():
+        print(f"Input file {input_file} does not exist.")
+        return
+    if not ref_file.exists():
+        print(f"Reference file {ref_file} does not exist.")
+        return
+
+    print(f"Registering {input_file.stem} to {ref_file.stem} => {output_file_name}")
+    # First stage: rigid registration (6 DOF)
+    execute_command([
+        "/usr/local/fsl/bin/flirt",
+        "-in", input_file.stem,
+        "-ref", ref_file,
+        "-omat", f"{output_file_name}_rigid.mat",
+        "-out", f"{output_file_name}_rigid",
+        "-dof", "6",                    # Rigid registration (rotation + translation)
+        "-cost", "normmi",              # Normalized mutual information
+        "-searchrx", "-180", "180",     # Full rotation search
+        "-searchry", "-180", "180",
+        "-searchrz", "-180", "180",
+        "-bins", "256",
+        "-interp", "spline"             # High quality interpolation
+    ])
+
+    # Second stage: affine registration (12 DOF) initialized with rigid result
+    execute_command([
+        "/usr/local/fsl/bin/flirt",
+        "-in", input_file.stem,
+        "-ref", ref_file,
+        "-init", f"{output_file_name}_rigid.mat",  # Initialize with rigid transform
+        "-omat", f"{output_file_name}.mat",
+        "-out", output_file_name,
+        "-dof", "12",                   # Affine registration
+        "-cost", "normmi",
+        "-bins", "256",
+        "-interp", "spline"
+    ])
+
+def register_ct_to_mni(input_file: Path, output_file_name: str) -> None:
+    """
+    Register a CT image to MNI space using FSL FLIRT.
+    
+    This function performs affine registration (12 DOF) of a CT image to the MNI152 T1 0.5mm template,
+    using normalized mutual information as the cost function and high quality sinc interpolation.
+    The registration allows for full rotation search to handle any initial orientation.
+    
+    Args:
+        input_file: Path to the input CT image
+        output_file_name: Name of the output file (without extension). Will create:
+            - {output_file_name}: Final registered image in MNI space
+            - {output_file_name}.mat: Affine transformation matrix from native to MNI space
+    """
+    input_file = Path(input_file)
+    ref_file = Path(f"{os.environ.get('FSLDIR')}/data/standard/MNI152_T1_0.5mm.nii.gz")
+
+    if not input_file.exists():
+        print(f"Input file {input_file} does not exist.")
+        return
+    if not ref_file.exists():
+        print(f"Reference file {ref_file} does not exist.")
+        return
+    
+    print(f"Registering {input_file.name} to MNI space => {output_file_name}")
+    execute_command([
+        "/usr/local/fsl/bin/flirt",
+        "-in", input_file.name,
+        "-ref", ref_file,
+        "-out", output_file_name,
+        "-omat", f"{output_file_name}.mat",
+        "-cost", "normmi",
+        "-dof", "12",
+        "-searchrx", "-180", "180",  # Full rotation range
+        "-searchry", "-180", "180",
+        "-searchrz", "-180", "180",
+        "-bins", "256",              # More bins for better intensity mapping
+        "-interp", "sinc"            # Higher quality interpolation
+    ])
+
+def register_to_mni_ants(input_file: Path, output_file_name: str, normalize: bool = True) -> None:
+    """
+    Register a single image (T1 or CT) to MNI space using ANTs
+    
+    Args:
+        input_file: Path to the input image (T1 or CT)
+        output_file_name: Name of the output file
+        normalize: Whether to normalize the image intensities before registration
+    """
+    input_file = Path(input_file)
+    ref_file = Path(f"{os.environ.get('FSLDIR')}/data/standard/MNI152_T1_1mm.nii.gz")
+    #ref_file = Path(f"MNI152_T1_1mm_brain_normalized.nii.gz")
+
+    if not input_file.exists():
+        print(f"Input file {input_file} does not exist.")
+        return
+    if not ref_file.exists():
+        print(f"Reference file {ref_file} does not exist.")
+        return
+
+    # Normalize image intensities if requested
+    if normalize:
+        norm_file_path = f"norm_{input_file.name}"
+        print(f"Normalizing {input_file.name}")
+        
+        # Get the statistics values first using subprocess directly
+        p2_value = subprocess.run(['fslstats', input_file.name, '-P', '2'], 
+                                capture_output=True, text=True, check=True).stdout.strip()
+        p98_value = subprocess.run(['fslstats', input_file.name, '-P', '98'], 
+                                 capture_output=True, text=True, check=True).stdout.strip()
+        
+        # Use the values in the normalization command
+        execute_command([
+            "fslmaths",
+            input_file.name,
+            "-sub", p2_value,
+            "-div", p98_value,
+            norm_file_path
+        ])
+        input_file = Path(norm_file_path)
+
+    print(f"Registering {input_file.stem} to {ref_file.stem} => {output_file_name}")
+    execute_command([
+        "antsRegistrationSyNQuick.sh",
+        "-d", "3",
+        "-f", ref_file,
+        "-m", input_file,
+        "-t", "r",
+        "-t", "a",
+        "-n", "12",
+        "-o", output_file_name
+    ])
+
+    # execute_command([
+    #     "antsRegistrationSyN.sh",
+    #     "-d", "3",
+    #     "-f", ref_file,
+    #     "-m", input_file,
+    #     "-t", "sr",
+    #     "-n", "12",
+    #     "-o", output_file_name,
+    # ])
+
+    # Convert ANTs matrix to FSL format
+    execute_command([
+        "ConvertTransformFile",
+        "3",
+        f"{output_file_name}0GenericAffine.mat",
+        f"{output_file_name}matrix.txt",
+        "--matrix"
+    ])
+
+    # Convert the transformation matrix to FSL format
+    print(f"Converting transformation matrix to FSL format")
+    ants_matrix_file = Path(f"{output_file_name}matrix.txt")
+    fsl_matrix_file = Path(f"{output_file_name}fsl.mat")
+
+    try:
+        # Read the ANTs matrix
+        with open(ants_matrix_file, 'r') as f:
+            lines = f.readlines()
+
+        # Parse the matrix values (skip header lines starting with #)
+        matrix_lines = [line for line in lines if not line.startswith('#')]
+        matrix = np.array([list(map(float, line.split())) for line in matrix_lines])
+
+        # Create 4x4 matrix
+        matrix_4x4 = np.eye(4)
+        matrix_4x4[:3, :3] = matrix[:3, :3]  # Copy rotation/scaling part
+        matrix_4x4[:3, 3] = matrix[:3, -1]   # Copy translation part
+
+        # Convert to FSL format
+        fsl_matrix = np.linalg.inv(matrix_4x4)
+
+        # Save in FSL format
+        np.savetxt(fsl_matrix_file, fsl_matrix, fmt='%g')
+        print(f"FSL matrix saved to {fsl_matrix_file}")
+
+    except Exception as e:
+        print(f"Error converting matrix to FSL format: {e}")
