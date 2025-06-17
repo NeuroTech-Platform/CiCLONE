f1d05df5c33bbac61885c3f98d070a7ffef3a171
Sipp Florian <florian.sipp@chuv.ch>
2025-06-17 11:24:12 +0200

    Crosshair thingy

--- ciclone/controllers/__init__.py
+++ ciclone/controllers/__init__.py
@@ -4,9 +4,11 @@ Controllers package for CiCLONE application.
 This package contains all controllers following the MVC pattern:
 - ElectrodeController: Coordinates electrode operations between models and views
 - ImageController: Manages image-related operations and view updates
+- CrosshairController: Coordinates crosshair operations between models and views
 """
 
 from .electrode_controller import ElectrodeController
 from .image_controller import ImageController
+from .crosshair_controller import CrosshairController
 
-__all__ = ['ElectrodeController', 'ImageController'] 
+__all__ = ['ElectrodeController', 'ImageController', 'CrosshairController'] 
--- ciclone/models/__init__.py
+++ ciclone/models/__init__.py
@@ -5,10 +5,12 @@ This package contains all data models following the MVC pattern:
 - ElectrodeModel: Manages electrode data and business logic
 - CoordinateModel: Handles electrode coordinate data
 - ImageModel: Manages image data and operations
+- CrosshairModel: Manages crosshair state and business logic
 """
 
 from .electrode_model import ElectrodeModel
 from .coordinate_model import CoordinateModel
 from .image_model import ImageModel
+from .crosshair_model import CrosshairModel
 
-__all__ = ['ElectrodeModel', 'CoordinateModel', 'ImageModel'] 
+__all__ = ['ElectrodeModel', 'CoordinateModel', 'ImageModel', 'CrosshairModel'] 
