648afa50c95a543f32d592d557d0a4f18d5826cb
floriansipp <72794896+floriansipp@users.noreply.github.com>
2025-10-16 21:05:19 +0200

    fix mni processing for mri's (#4)
    add an about window

--- ciclone/_metadata.py
+++ ciclone/_metadata.py
@@ -3,7 +3,7 @@ Single source of truth for application metadata.
 This file is used by the AboutDialog and can be imported by pyproject.toml.
 """
 
-__version__ = "2.0.0-alpha8"
+__version__ = "2.0.0-alpha9"
 __authors__ = ["Florian Sipp"]
 __author_email__ = "florian.sipp@chuv.ch"
 __copyright__ = "© 2025 CHUV - Lausanne University Hospital"
